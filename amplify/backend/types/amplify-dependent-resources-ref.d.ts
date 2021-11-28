8a0612a68a5de584ac056a9166f8b6775457d2e3
Thaer Jomhawi <thaer.fjomhawi@gmail.com>
2021-11-28 22:13:18 +0200

    lab32

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,8 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "thaertaskmaster": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
